4c59f1dcacaf91438fcb0068c41a7e634bf847e6
shobh <shobh@DESKTOP-94L5NBI>
2018-03-19 00:54:20 +0530

    Added admin panel UI

--- GisSurveyInformationApplication/src/main/java/oro/gis/controller/AdminController.java
+++ GisSurveyInformationApplication/src/main/java/oro/gis/controller/AdminController.java
@@ -18,6 +18,7 @@ public class AdminController
 {
 		
 	@RequestMapping(value="",method=RequestMethod.GET)
+	//Run for asking details
 	public ModelAndView index(Model model)
 	{
 		ModelAndView indexView = new ModelAndView();
@@ -27,18 +28,29 @@ public class AdminController
 	}
 	
 	@RequestMapping(value="", method=RequestMethod.POST)
+	// Run when
+		// 1. Accept credentials of admin
+		// 2. Redirect of Admin Panel occurs after getting logged in
 	public ModelAndView index(@ModelAttribute("adminDetails") AdminDetailsBean adminDetailsBean, HttpServletRequest request,HttpSession session)
 	{
 		ModelAndView indexView = new ModelAndView();
+		
+		//Run if already logged and clicked on dashBoard link on navigation bar
+		// so value stored in session cannot be null then
 		if((Boolean)session.getAttribute("logged")!=null)
 		{
 			indexView.setViewName("adminSide/adminPanel");
 		}
+		
+		//Run when given right credentials and then store value in session
 		else if(adminDetailsBean.verify())
 		{
+			//storing value in session for not asking again
 			session.setAttribute("logged", true);
 			indexView.setViewName("adminSide/adminPanel");
 		}
+		
+		//Run when wrong credentials are given
 		else
 		{
 			request.setAttribute("error","Wrong Details...");
